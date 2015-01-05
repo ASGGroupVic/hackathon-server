1d1628632eb41ad9e41e6c84274f933d68a4fdfd
Anthony Pasquale <anthony.pasquale@smsmt.com>
2015-01-05 18:07:59 +1100

    Refactored consultant methods to a module

--- api.js
+++ api.js
@@ -1,88 +1,20 @@
 'use strict';
 var restify = require('restify');
+var consultantSvc = require('./consultantSvc.js');
 
 function getPing(req, res, next) {
-    res.send('pong');
-    next();
-}
-
-var consultants = [{
-  name: 'test.consultant',
-  email: 'test.consultant@smsmt.com',
-  clients: [
-    {
-      name: 'TestClient'
-    },
-    {
-      name: 'telstra'
-    }
-  ]
-},
-{
-  name: 'david.carroll',
-  email: 'david.carroll@smsmt.com',
-  clients: [
-    {
-      name: 'eNett'
-    },
-    {
-      name: 'grv'
-    }
-
-  ]
-},
-{
-  name: 'anthony.pasquale',
-  email: 'anthony.pasquale@smsmt.com',
-  clients: [
-    {
-      name: 'smsbench'
-    },
-    {
-      name: 'anz'
-    }
-
-  ]
-}
-];
-
-function getClientsOfConsultant(req, res, next) {
-  var email = req.params.email;
-  console.log("Consultant: " + email);
-  var consultant = getConsultantFromRepo(email)
-  
-  res.send(consultant.clients);
+  res.send('pong');
   next();
 }
 
-function getConsultant(req, res, next) {
-  var email = req.params.email;
-  console.log("Consultant: " + email);
-  var consultant = getConsultantFromRepo(email)
-  
-  res.send(consultant);
-  next();
-}
-
-function getConsultantFromRepo(email)
-{
-  var consultant = {};
-  for (var i = consultants.length - 1; i >= 0; i--) {
-    if(consultants[i].email === email)
-    {
-      consultant = consultants[i];
-    }
-  }
-  return consultant;
-}
-
-function postMood(req, res, next)
-{
+function postMood(req, res, next) {
   console.log(req.body.mood);
   res.status(200);
   res.json({
     type: true
   });
+
+  next();
 }
 
 var server = restify.createServer();
@@ -90,13 +22,10 @@ var server = restify.createServer();
 server.use(restify.bodyParser());
 server.get('/v1/ping/', getPing);
 
-server.get('/v1/consultant/:email', getConsultant);
-server.get('/v1/consultant/:email/clients', getClientsOfConsultant);
+server.get('/v1/consultant/:email', consultantSvc.getConsultant);
+server.get('/v1/consultant/:email/clients', consultantSvc.getClientsOfConsultant);
 server.post('/v1/consultant/:email/mood', postMood);
 
-
-
-
-server.listen(8080, function() {
-    console.log('%s listening at %s', server.name, server.url);
+server.listen(8080, function () {
+  console.log('%s listening at %s', server.name, server.url);
 });
