d936801036b7fdf8da5e488f8ed2590015c28584
Anthony Pasquale <anthony.pasquale@smsmt.com>
2015-01-05 16:20:09 +1100

    Refactored the consultant service

--- api.js
+++ api.js
@@ -16,23 +16,31 @@ var consultants = [{
   ]
 }];
 
-function getConsultant(req, res, next) {
-    console.log("Consultant: " + req.params.id);
-    var consultant = {}
-    for (var i = consultants.length - 1; i >= 0; i--) {
-      if(consultants[i].name == req.params.id)
-      {
-        consultant = consultants[i];
-      }
-    };
-    res.send(consultant);
-    next();
+function getClientsOfConsultant(req, res, next) {
+  var email = req.params.email;
+  console.log("Consultant: " + email);
+  var consultant = getConsultantFromRepo(email)
+  
+  res.send(consultant.clients);
+  next();
+}
+
+function getConsultantFromRepo(email)
+{
+  var consultant = {};
+  for (var i = consultants.length - 1; i >= 0; i--) {
+    if(consultants[i].email == email)
+    {
+      consultant = consultants[i];
+    }
+  };
+  return consultant;
 }
 
 var server = restify.createServer();
 server.get('/ping/', getPing);
 
-server.get('/consultant/:id', getConsultant);
+server.get('/consultant/:email/clients', getClientsOfConsultant);
 
 server.listen(8080, function() {
     console.log('%s listening at %s', server.name, server.url);
