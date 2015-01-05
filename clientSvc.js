eacc58f8674310dbeb44cbbb1ce631d396afb806
Anthony Pasquale <anthony.pasquale@smsmt.com>
2015-01-06 10:13:19 +1100

    Cleaned up jslint

--- clientSvc.js
+++ clientSvc.js
@@ -1,26 +1,27 @@
 'use strict';
+/*global db*/
 
-function getMoodForClient(code, callback){
-  var mood = {};
-  
+function getMoodForClient(code, callback) {
   // NOT THE CORRECT QUERY YET. NEEDS TO CHECK RELATIONS
   // FOR MOOD. - JP
   var query = [
     'MATCH (n: `Client` {clientCode:{cCode}})  ',
     'RETURN n '
   ].join('\n');
-  
+
   var params = {
     cCode : code
   };
 
   db.query(query, params, function (err, results) {
-    if (err) throw err;
+    if (err) {
+      throw err;
+    }
     var mood = results.map(function (result) {
-      console.log(result['n']['data']);
-      return result['n']['data'];
+      console.log(result.n.data);
+      return result.n.data;
     });
-  	callback(mood);
+    callback(mood);
   });
 }
 
@@ -28,12 +29,12 @@ exports.getMood = function (req, res, next) {
 
   var code = req.params.code;
   console.log('Client: ' + code);
-  
-  getMoodForClient(code, function(mood){
-     console.log('Hooray! ' + mood);
 
-     res.send(mood);
-     next();
+  getMoodForClient(code, function (mood) {
+    console.log('Hooray! ' + mood);
+
+    res.send(mood);
+    next();
   });
 };
 
