420958aac81308b66534811053c4e38fc36ab18b
davejtcarroll <dave.jt.carroll@gmail.com>
2015-01-06 09:09:50 +1100

    test code for create/insert into database WARNINGS THE HACKS

--- consultantSvc.js
+++ consultantSvc.js
@@ -1,6 +1,7 @@
 'use strict';
 
 
+
 function getConsultantClientsFromRepo(email, callback) {
   var consultant = {};
 
@@ -8,15 +9,15 @@ function getConsultantClientsFromRepo(email, callback) {
     'MATCH (co:Consultant {email:{emailAddress}})-->(en:Engagement)-->(cl:Client)',
     'RETURN cl;'
   ].join('\n');
-  
+
   var params = {
     emailAddress: email
   };
   
-  db.query(query, params, function (err, results) {
+  db.query(query, null, function (err, results) {
     if (err) throw err;
     var clients = results.map(function (result) {
-      console.log(result["cl"]["data"]);
+      console.log(result["cl"]);
       return result["cl"]["data"];
     });
     callback(clients);
@@ -26,6 +27,58 @@ function getConsultantClientsFromRepo(email, callback) {
 exports.getClientsOfConsultant = function (req, res, next) {
   var email = req.params.email;
   console.log("Consultant: " + email);
+  var clients = getConsultantClientsFromRepo(email, function(consultant) {
+    res.send(consultant);
+    next();
+  });
+
+};
+
+
+function createMoodForConsultant(email, callback) {
+  var consultant = {};
+
+  var query = [
+    //'CREATE (:Consultant {firstName:"Dave", lastName:"Carroll", phone:"0400555666", email:"dave.carr@smsmt.com", employeeId:"007"});'
+    
+   // 'match (c:Consultant),(e:Engagement)',
+    //'where c.employeeId = "007" AND e.name="Contact Centre Optimisiation"',
+   // 'create (c)-[:engagedOn]->(e);'
+
+    'MATCH (mo:Mood {name: "indifferent"}), (cons:Consultant{email: "dave.carr@smsmt.com"}), (client:Client{clientCode: "123"})',
+'MERGE (day:Day {day: toInt("6"), month: toInt("1"), year:toInt("2015")})',
+'MERGE (month:Month {month: toInt("1"), year: toInt("2015")})',
+'MERGE (year:Year {year: toInt("2015")})',
+'MERGE (cal:Calendar {name: "root"})',
+'MERGE (day)-[:AT]-(month)',
+'MERGE (month)-[:AT]-(year)',
+'MERGE (year)-[:AT]-(cal)',
+'CREATE (senti:Sentiment{timeofday:"01:00:00 PM"})-[:AT]->(day)',
+'CREATE (senti)-[:EMOTION]->(mo)',
+'CREATE (senti)-[:TOWARDS]->(client)',
+'CREATE (cons)-[:FELT]->(senti);'
+  ].join('\n');
+
+
+console.log(query);
+
+  var params = {
+    emailAddress: email
+  };
+  
+  db.query(query, null, function (err, results) {
+    if (err) throw err;
+    var clients = results.map(function (result) {
+      console.log(result);
+      return result;
+    });
+    callback(clients);
+  });
+}
+
+exports.getClientsOfConsultant = function (req, res, next) {
+  var email = req.params.email;
+  console.log("Consultant-clients: " + email);
   var clients = getConsultantClientsFromRepo(email, function(consultant) {
     res.send(consultant);
     next();
