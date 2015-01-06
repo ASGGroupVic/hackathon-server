254896b336837578c77f27142ebf80c9ab516490
davejtcarroll <dave.jt.carroll@gmail.com>
2015-01-06 12:07:15 +1100

    new endpoint client/{code}/consultant

--- clientSvc.js
+++ clientSvc.js
@@ -1,5 +1,6 @@
 'use strict';
 /*global db*/
+/*jshint unused:req */
 
 function getMoodForClient(code, callback) {
   // NOT THE CORRECT QUERY YET. NEEDS TO CHECK RELATIONS
@@ -92,11 +93,46 @@ function getClientsFromRepo(callback) {
   });
 }
 
+/*jslint unparam: true*/
 exports.getClients = function (req, res, next) {
   console.log("Get Clients");
 
   getClientsFromRepo(function (clients) {
     res.send(clients);
+    next();
+  });
+};
+/*jslint unparam: false*/
+
+function getConsultantforClientsFromRepo(clientCode, callback) {
+  var query = [
+    'MATCH (co:Consultant)-->(en:Engagement)-->(cl:Client {clientCode:{cCode}})',
+    'RETURN co'
+  ].join('\n');
+
+  var params = {
+    cCode: clientCode
+  };
+
+
+  db.query(query, params, function (err, results) {
+    if (err) {
+      throw err;
+    }
+    var consultants = results.map(function (result) {
+      console.log(result.co.data);
+      return result.co.data;
+    });
+    callback(consultants);
+  });
+}
+
+exports.getConsultantsbyClientCode = function (req, res, next) {
+  var code = req.params.code;
+  console.log('Client: ' + code);
+
+  getConsultantforClientsFromRepo(code, function (consultants) {
+    res.send(consultants);
     next();
   });
 };
